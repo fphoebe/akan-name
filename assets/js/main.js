ea9ed88745a6a713809cbc5ed7af1e0bcc5e7787
jeffdnganga <jeffdaniels950@gmail.com>
2022-04-06 15:22:58 +0300

    Added descriptive text in output page

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,15 @@ let text = document.getElementById("text");
 
 let day;
 
+var weekday = new Array(7)
+weekday[0] = "Sunday"
+weekday[1] = "Monday"
+weekday[2] = "Tuesday"
+weekday[3] = "Wednesday"
+weekday[4] = "Thursday"
+weekday[5] = "Friday"
+weekday[6] = "Saturday"
+
 const getDate = () => {
     myDate.addEventListener("change", function (){
         var input = myDate.value 
@@ -16,38 +25,53 @@ const getDate = () => {
     })
 }
 
+// Gets the day and gender selected, and outputs the correct Akan name and descriptive text
 const dayOfTheWeek = () => {
     if (male.checked) {
         if (day == 0){
             document.getElementById("akan-name").innerHTML = "Kwasi";
+            document.getElementById("text").innerHTML = "Which means an African male born on a " + weekday[day]
         }else if (day == 1){
             document.getElementById("akan-name").innerHTML = "Kwadwo";
+            document.getElementById("text").innerHTML = "Which means an African male born on a " + weekday[day]
         }else if (day == 2){
             document.getElementById("akan-name").innerHTML = "Kwabena";
+            document.getElementById("text").innerHTML = "Which means an African male born on a " + weekday[day]
         }else if (day == 3){
             document.getElementById("akan-name").innerHTML = "Kwaku";
+            document.getElementById("text").innerHTML = "Which means an African male born on a " + weekday[day]
         }else if (day == 4){
             document.getElementById("akan-name").innerHTML = "Yaw";
+            document.getElementById("text").innerHTML = "Which means an African male born on a " + weekday[day]
         }else if (day == 5){
             document.getElementById("akan-name").innerHTML = "Kofi";
+            document.getElementById("text").innerHTML = "Which means an African male born on a " + weekday[day]
         }else{
             document.getElementById("akan-name").innerHTML = "Kwame";
+            document.getElementById("text").innerHTML = "Which means an African male born on a " + weekday[day]
         }
     }else if (female.checked) {
         if (day == 0){
             document.getElementById("akan-name").innerHTML = "Akosua";
+            document.getElementById("text").innerHTML = "Which means an African female born on a " + weekday[day]
         }else if (day == 1){
             document.getElementById("akan-name").innerHTML = "Adwoa";
+            document.getElementById("text").innerHTML = "Which means an African female born on a " + weekday[day]
         }else if (day == 2){
             document.getElementById("akan-name").innerHTML = "Abenaa";
+            document.getElementById("text").innerHTML = "Which means an African female born on a " + weekday[day]
         }else if (day == 3){
             document.getElementById("akan-name").innerHTML = "Akua";
+            document.getElementById("text").innerHTML = "Which means an African female born on a " + weekday[day]
         }else if (day == 4){
             document.getElementById("akan-name").innerHTML = "Yaa";
+            document.getElementById("text").innerHTML = "Which means an African female born on a " + weekday[day]
         }else if (day == 5){
             document.getElementById("akan-name").innerHTML = "Afua";
+            document.getElementById("text").innerHTML = "Which means an African female born on a " + weekday[day]
         }else{
             document.getElementById("akan-name").innerHTML = "Ama";
+            document.getElementById("text").innerHTML = "Which means an African female born on a " + weekday[day]
         }
     }
 }
